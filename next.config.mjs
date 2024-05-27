3545b93f82f8960c0fda2d4ef04fb94b3a93f828
kienbui195 <kaybi9x@gmail.com>
2024-05-27 17:51:32 +0700

    init

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  domain: ['127.0.0.1'],
+  images: {
+    remotePatterns: [],
+    loader: "default"
+  },
+  reactStrictMode: false
+};
+
+export default nextConfig;
