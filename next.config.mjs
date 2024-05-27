355d4a850bf7b5fd7ea39bc3c18ec15538fd4030
kienbui195 <kaybi9x@gmail.com>
2024-05-27 20:34:42 +0700

    fix file config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  domain: ['127.0.0.1'],
   images: {
+    domains: ['127.0.0.1', 'admin.kiendev.click'],
     remotePatterns: [],
     loader: "default"
   },
