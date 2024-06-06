138fabed7d079219f85be011cc7965e9694af38a
kienbui195 <kaybi9x@gmail.com>
2024-06-06 18:05:21 +0700

    update

--- src/lib/apis/index.ts
+++ src/lib/apis/index.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const apis = {
   login: async ({ email, password }: { email: string; password: string }) => {
+    const isClient = typeof window === 'object'
+    const token = isClient ? localStorage.getItem('KDEV_USER') ? '' : '' : ''
+
     return await axios
       .post(`${process.env.NEXT_PUBLIC_BE}/auth/local`, {
         identifier: email,
